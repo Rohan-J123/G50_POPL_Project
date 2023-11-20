880555df278f0b131911b86061f3a531d89c49db
Rohan-J123 <139244765+Rohan-J123@users.noreply.github.com>
2023-11-20 19:06:20 +0530

    Minor Changes to Program.cs

--- C_Sharp_Concepts/C_Sharp_Concepts/Program.cs
+++ C_Sharp_Concepts/C_Sharp_Concepts/Program.cs
@@ -22,7 +22,7 @@ class BubbleSort
 
         for (int i = 0; i < n - 1; i++)
         {
-            for (int j = 0; j < n - i + 10; j++)
+            for (int j = 0; j < n - i - 1; j++)
             {
                 // Swap if the element found is greater
                 // than the next element
